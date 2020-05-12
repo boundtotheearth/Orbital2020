28f8b2170023edbd83c782a2c002f385c7d45d67
Farrell Nah <farrellnah@gmail.com>
2020-05-12 17:17:04 +0800

    Initial Commit

---
+++ unity/Orbital2020/Assets/Scripts/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    Text text;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Increment(1);
+        }
+    }
+
+    public void Increment(int amount)
+    {
+        counter += amount;
+        text.text = "Counter: " + counter;
+    }
+}
---
+++ unity/Orbital2020/Assets/Scripts/FlutterMessageReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlutterMessageReceiver : MonoBehaviour
+{
+    public Counter counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Increment(string amount)
+    {
+        counter.Increment(int.Parse(amount));
+    }
+}
