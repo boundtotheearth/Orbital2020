540b30ac61f1d826c684519aecefd6e384dd8420
shadowezz <wenjie.cao@hotmail.com>
2020-05-20 18:05:03 +0800

    pass User data instead of FirebaseUser after logging in

--- lib/Root.dart
+++ lib/Root.dart
@@ -4,19 +4,20 @@ import 'package:orbital2020/Login.dart';
 import 'package:provider/provider.dart';
 import 'Auth.dart';
 import 'AuthProvider.dart';
+import 'DataContainers/User.dart';
 import 'HomePage.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Auth auth = AuthProvider.of(context).auth;
-    return StreamBuilder<FirebaseUser>(
+    return StreamBuilder<User>(
       stream: auth.onAuthStateChanged,
-      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final bool isLoggedIn = snapshot.hasData;
           return isLoggedIn
-            ? Provider<FirebaseUser>(
+            ? Provider<User>(
                 create: (_) => snapshot.data,
                 child: HomePage()
               )
