6526c68eb59e7d8324605edad4a851375fa84bbf
shadowezz <wenjie.cao@hotmail.com>
2020-07-05 20:29:19 +0800

    student notif when assigned task

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,51 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+export const assignedTask = functions.firestore
+    .document("students/{studentId}/tasks/{taskId}")
+    .onCreate(async (snapshot, context) => {
+
+        const studentId = context.params.studentId;
+        console.log(studentId);
+        const task = await db.collection("tasks")
+            .doc(context.params.taskId)
+            .get();
+        if (task.data()?.createdById === studentId) {
+            console.log("self created task");
+            return ;
+        }
+        console.log(task.data()?.createdByName);
+        console.log(task.data()?.name);
+        
+        const device_token = await db.collection("accountTypes")
+            .doc(studentId)
+            .get()
+            .then((doc) => doc.data()?.token);
+        
+        console.log(device_token);
+
+        
+        const payload: admin.messaging.MessagingPayload = {
+            notification: {
+                title: "You have a new task!",
+                body: `${task.data()?.createdByName} has assigned you a new task: ${task.data()?.name}`,
+                sound: "default"
+            },
+            data: {
+                click_action: "FLUTTER_NOTIFICATION_CLICK"
+            }
+        }
+
+        try {
+            await fcm.sendToDevice(device_token, payload);
+            console.log("Notification sent successfully!");
+        } catch (err) {
+            console.log("Error sending Notification: " + err);
+        }
+    })
+
+
